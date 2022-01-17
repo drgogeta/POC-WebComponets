2895910eace9a45f2d5417dbeb66011d168f03be
Daniel Castellanos <daniel.castellanosa@perficient.com>
2022-01-17 14:41:09 -0500

    push to pages

--- README.md
+++
@@ -1,2 +0,0 @@
-# POC-WebComponets
-this the example for integrate React witch referents web-components that management Rick and Morty api and Pokemon Api
--- app-2/src/services/api.service.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-export const getPokemons = () => {
-    return axios.get('https://pokeapi.co/api/v2/pokemon?limit=100&offset=200')
-}
-
-export const getPokemon = (id) => {
-    return axios.get(`https://pokeapi.co/api/v2/pokemon/${id}`);
-}
